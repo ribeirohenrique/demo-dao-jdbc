c824fae93b9ceb8f8d423708d3c831d1537e978f
Henrique Mendes <henrique.r.mendes@accenture.com>
2022-05-12 14:54:39 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.util.Date;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.sql.*;
@@ -15,6 +18,8 @@ public class Program {
         Seller seller = new Seller(1, "Fulano", "fulano@gmail.com", new Date(), 2000.00, obj);
 
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
     }
 
